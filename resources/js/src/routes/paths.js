ffdfcc5f488c2dc4c9d8156c57e8530d634812e9
lazaraslobo <lazaraslobo@gmail.com>
2024-01-30 09:11:10 +0530

    route added

--- resources/js/src/routes/paths.js
+++ resources/js/src/routes/paths.js
@@ -1,11 +1,11 @@
 const appBaseUrl = import.meta.env.VITE_APP_URL;
 
-const adminRoutePrefix = `/admin`
-const clientRoutePrefix = `/`
+export const adminRoutePrefix = `/admin`
+export const clientRoutePrefix = `/`
 
 export const adminRoutes = {
     admin_home: `${adminRoutePrefix}`,
-    create_new_cms_page: `${adminRoutePrefix}`
+    create_new_cms_page: `${adminRoutePrefix}/cms/create`
 }
 
 
--- resources/js/src/routes/router.ts
+++ resources/js/src/routes/router.ts
@@ -1,12 +1,24 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import CreateCmsPage from '../components/admin/CreatePage.vue';
-import {clientRoutes, adminRoutes} from './paths.js';
+import {clientRoutes, adminRoutes, adminRoutePrefix, clientRoutePrefix} from './paths.js';
 
 const routes: RouteRecordRaw[] = [
     {
+        path: adminRoutePrefix,
+        name: 'admin-routes',
         path: adminRoutes.admin_home,
-        name: 'admin-home-route',
-        component: CreateCmsPage,
+        children: [
+            {
+                path: adminRoutes.admin_home,
+                name: 'admin-home-route',
+                redirect: adminRoutes.create_new_cms_page
+            },
+            {
+                path: adminRoutes.create_new_cms_page,
+                name: 'admin-create-cms-page-route',
+                component: CreateCmsPage
+            }
+        ]
     }
 ];
 
