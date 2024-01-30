ae8a8f662f8e6dc3553110c89f776d3c38035e75
lazaraslobo <lazaraslobo@gmail.com>
2024-01-30 13:17:00 +0530

    slug fixes

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,13 +6,13 @@ use App\Models\Pages;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class PagesController extends Controller
 {
     public function __construct(
         protected Pages $pages
-    )
-    {
+    ) {
     }
 
     public function getAllPagesList()
@@ -23,32 +23,33 @@ class PagesController extends Controller
 
     public function updateOrCreatePageContent(
         Request $request
-    ){
-//        $rules = [
-//            'title' => 'required|string|max:255',
-//            'content' => 'required|string',
-//            'slug' => 'required|string|max:255',
-//            'parent_id' => 'required|number',
-//            'id' => ''
-//        ];
-//
-//        $messages = [
-//            'title.required' => 'The title field is required.',
-//            'content.required' => 'The content field is required.',
-//            'slug.required' => 'The slug field is required.',
-//            'parent_id.required' => 'The parent_id field is required.',
-//        ];
-//
-//        $validator = Validator::make($request->all(), $rules, $messages);
-//
-//        if ($validator->fails()) {
-//            return response()->json(['errors' => $validator->errors()], 422);
-//        }
-
-        $this->pages::updateOrCreate(
-            ['id' => $request->only(['id'])],
-            $request->only(['title', 'content', 'slug'])
-        );
+    ) {
+        //        $rules = [
+        //            'title' => 'required|string|max:255',
+        //            'content' => 'required|string',
+        //            'slug' => 'required|string|max:255',
+        //            'parent_id' => 'required|number',
+        //            'id' => ''
+        //        ];
+        //
+        //        $messages = [
+        //            'title.required' => 'The title field is required.',
+        //            'content.required' => 'The content field is required.',
+        //            'slug.required' => 'The slug field is required.',
+        //            'parent_id.required' => 'The parent_id field is required.',
+        //        ];
+        //
+        //        $validator = Validator::make($request->all(), $rules, $messages);
+        //
+        //        if ($validator->fails()) {
+        //            return response()->json(['errors' => $validator->errors()], 422);
+        //        }
+        $attr = $request->only(['title', 'content']);
+        $attr['slug'] = Str::slug($request->get('slug'));
+         $this->pages::updateOrCreate(
+             ['id' => $request->only(['id'])],
+             $attr
+         );
 
         return response()->json(['data' => []]);
     }
